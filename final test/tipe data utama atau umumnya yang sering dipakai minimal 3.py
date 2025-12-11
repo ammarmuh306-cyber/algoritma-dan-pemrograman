0af27d74fd467e3602cad1288a9be5c3b43b73f5
ammarmuh306-cyber <ammarmuh306@gmail.com>
2025-12-11 16:37:20 +0800

    Create tipe data utama atau umumnya yang sering dipakai minimal 3.py

---
+++ final test/tipe data utama atau umumnya yang sering dipakai minimal 3.py
@@ -0,0 +1,24 @@
+# tipe_data.py
+
+# Tipe Data Utama yang Sering Dipakai
+
+# 1. Integer (bilangan bulat)
+umur = 18
+print("Integer (umur):", umur, type(umur))
+
+# 2. Float (bilangan desimal)
+tinggi = 158.5
+print("Float (tinggi):", tinggi, type(tinggi))
+
+# 3. String (teks)
+nama = "muh ammar s"
+print("String (nama):", nama, type(nama))
+
+# (Opsional Tambahan)
+# 4. Boolean (True/False)
+is_active = True
+print("Boolean (is_active):", is_active, type(is_active))
+
+# 5. List (kumpulan data)
+nilai = [80, 85, 90]
+print("List (nilai):", nilai, type(nilai))
